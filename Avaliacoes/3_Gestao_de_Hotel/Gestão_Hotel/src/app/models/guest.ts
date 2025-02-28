4bf658eb661d4da7f4073e3fe4b3f74b4306e91b
Batatexy <bielcarrascosa@gmail.com>
2025-02-28 17:18:17 -0300

    Criação de Branch do projeto de Calculadora feito em Angular

--- Avaliacoes/3_Gestao_de_Hotel/Gestão_Hotel/src/app/models/guest.ts
+++
@@ -1,7 +0,0 @@
-export type Guest = {
-    id?: string;
-    name: string;
-    email: string;
-    phone: string;
-    document: string;
-};
--- Exercicios/4_Angular/16_Filmes/src/app/models/credits.ts
+++
@@ -1,8 +0,0 @@
-import { Cast } from "./cast";
-import { Crew } from "./crew";
-
-export type Credits = {
-    id: number;
-    cast: Array<Cast>;
-    crew: Array<Crew>;
-};
