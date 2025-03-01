47abbebd600c12d2bdf371cf1edccedd4c5afc2d
Batatexy <bielcarrascosa@gmail.com>
2025-02-28 21:22:51 -0300

    Criação de branch de site Pokedex feito em Angular

--- Avaliacoes/3_Gestao_de_Hotel/Gestão_Hotel/src/app/models/room.ts
+++
@@ -1,6 +0,0 @@
-export type Room = {
-    id: number;
-    name: string;
-    amount: number,
-    maxGuests: number,
-};
--- Exercicios/0_Templates/index.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("").addEventListener("click", () =>
-{
-
-});
